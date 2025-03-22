6e5d339d758c3e4d02e9ffc705da4e7e5a035762
Khang Chinh <luckydoom000@gmail.com>
2025-03-22 11:11:18 +0700

    add cors

--- src/server.js
+++ src/server.js
@@ -2,11 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine"
 import initWebRoutes from "./route/web";
+import cors from "cors"
 import connectDB from "./config/connectDB"
 require('dotenv').config();
 
 let app = express();
 
+// app.use(cors({ origin: true }));
+app.use(cors({
+    origin: 'http://localhost:3000', // Chỉ định rõ origin của frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true // Cho phép credentials
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
